f02a0e3423ac16bf724315471304ec9ec0e828f1
mataha <mataha@users.noreply.github.com>
2022-06-04 04:20:33 +0200

    feat(yarn): execute from Yarn wrapper if it exists

--- plugins/yarn/yarn.plugin.zsh
+++ plugins/yarn/yarn.plugin.zsh
@@ -1,3 +1,21 @@
+function yarn-or-yarnw() {
+  local dir="$PWD" project_root="$PWD"
+  while [[ "$dir" != / ]]; do
+    if [[ -f "$dir/.yarnrc.yml" || -f "$dir/.yarnrc" || -f "$dir/yarnw" ]]; then
+      project_root="$dir"
+      break
+    fi
+    dir="${dir:h}"
+  done
+
+  if [[ -f "$project_root/yarnw" ]]; then
+    echo "executing yarnw instead of yarn"
+    "$project_root/yarnw" "$@"
+  else
+    command yarn "$@"
+  fi
+}
+
 if zstyle -T ':omz:plugins:yarn' global-path; then
   # Skip yarn call if default global bin dir exists
   [[ -d "$HOME/.yarn/bin" ]] && bindir="$HOME/.yarn/bin" || bindir="$(yarn global bin 2>/dev/null)"
@@ -10,35 +28,38 @@ if zstyle -T ':omz:plugins:yarn' global-path; then
   unset bindir
 fi
 
-alias y="yarn"
-alias ya="yarn add"
-alias yad="yarn add --dev"
-alias yap="yarn add --peer"
-alias yb="yarn build"
-alias ycc="yarn cache clean"
-alias yd="yarn dev"
-alias yga="yarn global add"
-alias ygls="yarn global list"
-alias ygrm="yarn global remove"
-alias ygu="yarn global upgrade"
-alias yh="yarn help"
-alias yi="yarn init"
-alias yin="yarn install"
-alias yln="yarn lint"
-alias ylnf="yarn lint --fix"
-alias yls="yarn list"
-alias yout="yarn outdated"
-alias yp="yarn pack"
-alias yrm="yarn remove"
-alias yrun="yarn run"
-alias ys="yarn serve"
-alias yst="yarn start"
-alias yt="yarn test"
-alias ytc="yarn test --coverage"
-alias yuc="yarn global upgrade && yarn cache clean"
-alias yui="yarn upgrade-interactive"
-alias yuil="yarn upgrade-interactive --latest"
-alias yup="yarn upgrade"
-alias yv="yarn version"
-alias yw="yarn workspace"
-alias yws="yarn workspaces"
+alias y="yarn-or-yarnw"
+alias ya="yarn-or-yarnw add"
+alias yad="yarn-or-yarnw add --dev"
+alias yap="yarn-or-yarnw add --peer"
+alias yb="yarn-or-yarnw build"
+alias ycc="yarn-or-yarnw cache clean"
+alias yd="yarn-or-yarnw dev"
+alias yga="yarn-or-yarnw global add"
+alias ygls="yarn-or-yarnw global list"
+alias ygrm="yarn-or-yarnw global remove"
+alias ygu="yarn-or-yarnw global upgrade"
+alias yh="yarn-or-yarnw help"
+alias yi="yarn-or-yarnw init"
+alias yin="yarn-or-yarnw install"
+alias yln="yarn-or-yarnw lint"
+alias ylnf="yarn-or-yarnw lint --fix"
+alias yls="yarn-or-yarnw list"
+alias yout="yarn-or-yarnw outdated"
+alias yp="yarn-or-yarnw pack"
+alias yrm="yarn-or-yarnw remove"
+alias yrun="yarn-or-yarnw run"
+alias ys="yarn-or-yarnw serve"
+alias yst="yarn-or-yarnw start"
+alias yt="yarn-or-yarnw test"
+alias ytc="yarn-or-yarnw test --coverage"
+alias yuc="yarn-or-yarnw global upgrade && yarn-or-yarnw cache clean"
+alias yui="yarn-or-yarnw upgrade-interactive"
+alias yuil="yarn-or-yarnw upgrade-interactive --latest"
+alias yup="yarn-or-yarnw upgrade"
+alias yv="yarn-or-yarnw version"
+alias yw="yarn-or-yarnw workspace"
+alias yws="yarn-or-yarnw workspaces"
+
+alias yarn="yarn-or-yarnw"
+compdef _yarn yarn-or-yarnw
